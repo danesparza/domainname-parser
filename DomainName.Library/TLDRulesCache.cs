be87f408346908d360925c4bcaa7eb9bbdbc902f
Dan Esparza <esparza.dan@gmail.com>
2013-01-24 01:20:05 -0500

    Initial commit

---
+++ DomainName.Library/TLDRulesCache.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using DomainName.Library.Properties;
+using System.Diagnostics;
+
+namespace DomainName.Library
+{
+    public sealed class TLDRulesCache
+    {
+        private static volatile TLDRulesCache _uniqueInstance;
+        private static object _syncObj = new object();
+        private static object _syncList = new object();
+        private List<TLDRule> _lstTLDRules;
+
+        private TLDRulesCache()
+        {
+            //  Initialize our internal list:
+            _lstTLDRules = GetTLDRules();
+            _lstTLDRules.Sort();
+        }
+
+        /// <summary>
+        /// Returns the singleton instance of the class
+        /// </summary>
+        public static TLDRulesCache Instance
+        {
+            get
+            {
+                if (_uniqueInstance == null)
+                {
+                    lock (_syncObj)
+                    {
+                        if (_uniqueInstance == null)
+                            _uniqueInstance = new TLDRulesCache();
+                    }
+                }
+                return (_uniqueInstance);
+            }
+        }
+
+        /// <summary>
+        /// List of TLD rules
+        /// </summary>
+        public List<TLDRule> TLDRuleList
+        {
+            get
+            {
+                return _lstTLDRules;
+            }
+            set
+            {
+                _lstTLDRules = value;
+            }
+        }
+
+        /// <summary>
+        /// Resets the singleton class and flushes all the cached 
+        /// values so they will be re-cached the next time they are requested
+        /// </summary>
+        public static void Reset()
+        {
+            lock (_syncObj)
+            {
+                _uniqueInstance = null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the list of TLD rules from the cache
+        /// </summary>
+        /// <returns></returns>
+        private List<TLDRule> GetTLDRules()
+        {
+            List<TLDRule> results = new List<TLDRule>();
+
+            //  If the cached suffix rules file exists...
+            if (File.Exists(Settings.Default.SuffixRulesFileLocation))
+            {
+                //  Load the rules from the cached text file
+                List<string> lstTLDRuleStrings = File.ReadAllLines(Settings.Default.SuffixRulesFileLocation, Encoding.UTF8).ToList();
+
+                //  Strip out any lines that are:
+                //  a.) A comment
+                //  b.) Blank
+                IEnumerable<TLDRule> lstTLDRules = from ruleString in lstTLDRuleStrings
+                                                   where
+                                                   !ruleString.StartsWith("//", StringComparison.InvariantCultureIgnoreCase)
+                                                   &&
+                                                   !(ruleString.Trim().Length == 0)
+                                                   select new TLDRule(ruleString);
+
+                //  Transfer this list to the results:
+                results = lstTLDRules.ToList();
+            }
+
+            //  Return our results:
+            Debug.WriteLine(
+                   string.Format("Loaded {0} rules into cache.", results.Count)
+                   );
+            return results;
+        }
+    }
+}
---
+++ DomainName.Tests/DomainTests.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace DomainName.Tests
+{
+    /// <summary>
+    /// Summary description for DomainTests
+    /// </summary>
+    [TestClass]
+    public class DomainTests
+    {
+        private DomainName.Library.DomainName outDomain = null;
+
+        public DomainTests()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void ParseNormalDomain()
+        {
+            //  Try parsing a 'normal' domain:
+            DomainName.Library.DomainName.TryParse("photos.totallycool.com", out outDomain);
+
+            //  The domain should be parsed as 'totallycool'
+            Assert.AreEqual<string>("totallycool", outDomain.Domain);
+
+            Debug.WriteLine(
+                string.Format("Looks like the parsed domain part is: {0}", outDomain.Domain)
+                );
+        }
+
+        [TestMethod]
+        public void ParseWildcardDomain()
+        {
+            //  Try parsing a 'wildcard' domain
+            if (DomainName.Library.DomainName.TryParse("photos.verybritish.co.uk", out outDomain))
+            {
+                //  The domain should be parsed as 'verybritish'
+                Assert.AreEqual<string>("verybritish", outDomain.Domain);
+
+                //  The TLD is 'co.uk'
+                Assert.AreEqual<string>("co.uk", outDomain.TLD);
+
+                //  The SLD is just an alias for 'Domain':
+                Assert.AreEqual<string>(outDomain.Domain, outDomain.SLD);
+
+                //  The subdomain is everything else to the left of the domain:
+                Assert.AreEqual<string>("photos", outDomain.SubDomain);
+            }
+            else
+            {
+                Debug.WriteLine("Apparently, we couldn't parse photos.verybritish.co.uk");
+            }
+        }
+
+        [TestMethod]
+        public void ParseExceptionDomain()
+        {
+            //  Try parsing an 'exception' domain
+            DomainName.Library.DomainName.TryParse("photos.wishlist.parliament.uk", out outDomain);
+
+            //  The domain should be parsed as 'parliament'
+            Assert.AreEqual<string>("parliament", outDomain.Domain);
+
+            Debug.WriteLine(
+                string.Format("Looks like the parsed domain part is: {0}", outDomain.Domain)
+                );
+        }
+
+    }
+}

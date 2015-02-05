4ac5d1de0c5ff83d38551337e02ad28f6b02a9e1
Chet Husk <chet_husk@volusion.com>
2015-02-05 11:10:18 -0600

    Add support for fetching the list of rules directly if not configured in App.Config
    Updates the project to .net 4.0 so that we can use the
    System.Net.Http.HttpClient class to get the response stream.

--- DomainName.Library/TLDRulesCache.cs
+++ DomainName.Library/TLDRulesCache.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.IO;
 using DomainName.Library.Properties;
 using System.Diagnostics;
+using System.Net.Http;
 
 namespace DomainName.Library
 {
@@ -76,31 +77,45 @@ namespace DomainName.Library
         {
             List<TLDRule> results = new List<TLDRule>();
 
-            //  If the cached suffix rules file exists...
+            var ruleStrings = ReadRulesData();           
+
+            //  Strip out any lines that are:
+            //  a.) A comment
+            //  b.) Blank
+            IEnumerable<TLDRule> lstTLDRules = from ruleString in ruleStrings
+                                               where
+                                               !ruleString.StartsWith("//", StringComparison.InvariantCultureIgnoreCase)
+                                               &&
+                                               !(ruleString.Trim().Length == 0)
+                                               select new TLDRule(ruleString);
+
+            //  Transfer this list to the results:
+            results = lstTLDRules.ToList();
+
+            //  Return our results:
+            Debug.WriteLine(string.Format("Loaded {0} rules into cache.", results.Count));
+            return results;
+        }
+
+        private IEnumerable<string> ReadRulesData()
+        {
             if (File.Exists(Settings.Default.SuffixRulesFileLocation))
             {
                 //  Load the rules from the cached text file
-                List<string> lstTLDRuleStrings = File.ReadAllLines(Settings.Default.SuffixRulesFileLocation, Encoding.UTF8).ToList();
-
-                //  Strip out any lines that are:
-                //  a.) A comment
-                //  b.) Blank
-                IEnumerable<TLDRule> lstTLDRules = from ruleString in lstTLDRuleStrings
-                                                   where
-                                                   !ruleString.StartsWith("//", StringComparison.InvariantCultureIgnoreCase)
-                                                   &&
-                                                   !(ruleString.Trim().Length == 0)
-                                                   select new TLDRule(ruleString);
-
-                //  Transfer this list to the results:
-                results = lstTLDRules.ToList();
+                foreach (var line in File.ReadAllLines(Settings.Default.SuffixRulesFileLocation, Encoding.UTF8))
+                    yield return line;
             }
-
-            //  Return our results:
-            Debug.WriteLine(
-                   string.Format("Loaded {0} rules into cache.", results.Count)
-                   );
-            return results;
+            else
+            {
+                // read the files from the web directly.
+                var datFile = new HttpClient().GetStreamAsync("https://publicsuffix.org/list/effective_tld_names.dat").Result;
+                using (var reader = new StreamReader(datFile))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                        yield return line;
+                }
+            }
         }
     }
 }

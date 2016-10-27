3279c1d5a873b96b9ec6635540c61d7d0772f02d
Dan Esparza <esparza.dan@gmail.com>
2016-10-27 15:05:20 -0400

    Library and namespace refactor
    Refactored library and namespace names for #7

---
+++ DomainParser.Library/DomainName.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+
+namespace DomainParser.Library
+{
+    public class DomainName
+    {
+        #region Private members
+
+        private string _subDomain = string.Empty;
+        private string _domain = string.Empty;
+        private string _tld = string.Empty;
+        private TLDRule _tldRule = null;
+
+        #endregion
+
+        #region Public properties
+
+        /// <summary>
+        /// The subdomain portion
+        /// </summary>
+        public string SubDomain
+        {
+            get
+            {
+                return _subDomain;
+            }
+        }
+
+        /// <summary>
+        /// The domain name portion, without the subdomain or the TLD
+        /// </summary>
+        public string Domain
+        {
+            get
+            {
+                return _domain;
+            }
+        }
+
+        /// <summary>
+        /// The domain name portion, without the subdomain or the TLD
+        /// </summary>
+        public string SLD
+        {
+            get
+            {
+                return _domain;
+            }
+        }
+
+        /// <summary>
+        /// The TLD portion
+        /// </summary>
+        public string TLD
+        {
+            get
+            {
+                return _tld;
+            }
+        }
+
+        /// <summary>
+        /// The matching TLD rule
+        /// </summary>
+        public TLDRule TLDRule
+        {
+            get
+            {
+                return _tldRule;
+            }
+        }
+
+        #endregion
+
+        #region Construction
+
+        /// <summary>
+        /// Constructs a DomainName object from the string representation of a domain. 
+        /// </summary>
+        /// <param name="domainString"></param>
+        public DomainName(string domainString)
+        {
+            //  If an exception occurs it should bubble up past this
+            ParseDomainName(domainString, out _tld, out _domain, out _subDomain, out _tldRule);
+        }
+
+        /// <summary>
+        /// Constructs a DomainName object from its 3 parts
+        /// </summary>
+        /// <param name="TLD">The top-level domain</param>
+        /// <param name="SLD">The second-level domain</param>
+        /// <param name="SubDomain">The subdomain portion</param>
+        /// <param name="TLDRule">The rule used to parse the domain</param>
+        private DomainName(string TLD, string SLD, string SubDomain, TLDRule TLDRule)
+        {
+            this._tld = TLD;
+            this._domain = SLD;
+            this._subDomain = SubDomain;
+            this._tldRule = TLDRule;
+        }
+
+        #endregion
+
+        #region Parse domain - private static method
+
+        /// <summary>
+        /// Converts the string representation of a domain to it's 3 distinct components: 
+        /// Top Level Domain (TLD), Second Level Domain (SLD), and subdomain information
+        /// </summary>
+        /// <param name="domainString">The domain to parse</param>
+        /// <param name="TLD"></param>
+        /// <param name="SLD"></param>
+        /// <param name="SubDomain"></param>
+        /// <param name="MatchingRule"></param>
+        private static void ParseDomainName(string domainString, out string TLD, out string SLD, out string SubDomain, out TLDRule MatchingRule)
+        {
+            TLD = string.Empty;
+            SLD = string.Empty;
+            SubDomain = string.Empty;
+            MatchingRule = null;
+
+            //  If the fqdn is empty, we have a problem already
+            if (domainString.Trim() == string.Empty)
+                throw new ArgumentException("The domain cannot be blank");
+
+            //  Next, find the matching rule:
+            MatchingRule = FindMatchingTLDRule(domainString);
+
+            //  At this point, no rules match, we have a problem
+            if(MatchingRule == null)
+                throw new FormatException("The domain does not have a recognized TLD");
+
+            //  Based on the tld rule found, get the domain (and possibly the subdomain)
+            string tempSudomainAndDomain = string.Empty;
+            int tldIndex = 0;
+            
+            //  First, determine what type of rule we have, and set the TLD accordingly
+            switch (MatchingRule.Type)
+            {
+                case TLDRule.RuleType.Normal:
+                    tldIndex = domainString.LastIndexOf("." + MatchingRule.Name, StringComparison.InvariantCultureIgnoreCase);
+                    tempSudomainAndDomain = domainString.Substring(0, tldIndex);
+                    TLD = domainString.Substring(tldIndex + 1);
+                    break;
+                case TLDRule.RuleType.Wildcard:
+                    //  This finds the last portion of the TLD...
+                    tldIndex = domainString.LastIndexOf("." + MatchingRule.Name, StringComparison.InvariantCultureIgnoreCase);
+                    tempSudomainAndDomain = domainString.Substring(0, tldIndex);
+
+                    //  But we need to find the wildcard portion of it:
+                    tldIndex = tempSudomainAndDomain.LastIndexOf(".", StringComparison.InvariantCultureIgnoreCase);
+                    tempSudomainAndDomain = domainString.Substring(0, tldIndex);
+                    TLD = domainString.Substring(tldIndex + 1);
+                    break;
+                case TLDRule.RuleType.Exception:
+                    tldIndex = domainString.LastIndexOf(".", StringComparison.InvariantCultureIgnoreCase);
+                    tempSudomainAndDomain = domainString.Substring(0, tldIndex);
+                    TLD = domainString.Substring(tldIndex + 1);
+                    break;
+            }
+
+            //  See if we have a subdomain:
+            List<string> lstRemainingParts = new List<string>(tempSudomainAndDomain.Split('.'));
+
+            //  If we have 0 parts left, there is just a tld and no domain or subdomain
+            //  If we have 1 part, it's the domain, and there is no subdomain
+            //  If we have 2+ parts, the last part is the domain, the other parts (combined) are the subdomain
+            if (lstRemainingParts.Count > 0)
+            {
+                //  Set the domain:
+                SLD = lstRemainingParts[lstRemainingParts.Count - 1];
+
+                //  Set the subdomain, if there is one to set:
+                if (lstRemainingParts.Count > 1)
+                {
+                    //  We strip off the trailing period, too
+                    SubDomain = tempSudomainAndDomain.Substring(0, tempSudomainAndDomain.Length - SLD.Length - 1);
+                }
+            }
+        }
+
+        #endregion
+
+        #region TryParse method(s)
+
+        /// <summary>
+        /// Converts the string representation of a domain to its DomainName equivalent.  A return value
+        /// indicates whether the operation succeeded.
+        /// </summary>
+        /// <param name="domainString"></param>
+        /// <param name="result"></param>
+        /// <returns></returns>
+        public static bool TryParse(string domainString, out DomainName result)
+        {
+            bool retval = false;
+
+            //  Our temporary domain parts:
+            string _tld = string.Empty;
+            string _sld = string.Empty;
+            string _subdomain = string.Empty;
+            TLDRule _tldrule = null;
+            result = null;
+
+            try
+            {
+                //  Try parsing the domain name ... this might throw formatting exceptions
+                ParseDomainName(domainString, out _tld, out _sld, out _subdomain, out _tldrule);
+
+                //  Construct a new DomainName object and return it
+                result = new DomainName(_tld, _sld, _subdomain, _tldrule);
+
+                //  Return 'true'
+                retval = true;
+            }
+            catch
+            {
+                //  Looks like something bad happened -- return 'false'
+                retval = false;
+            }
+
+            return retval;
+        }
+
+        #endregion
+
+        #region Rule matching
+        /// <summary>
+        /// Finds matching rule for a domain.  If no rule is found, 
+        /// returns a null TLDRule object
+        /// </summary>
+        /// <param name="domainString"></param>
+        /// <returns></returns>
+        private static TLDRule FindMatchingTLDRule(string domainString)
+        {
+            //  Split our domain into parts (based on the '.')
+            //  ...Put these parts in a list
+            //  ...Make sure these parts are in reverse order (we'll be checking rules from the right-most pat of the domain)
+            List<string> lstDomainParts = domainString.Split('.').ToList<string>();
+            lstDomainParts.Reverse();
+
+            //  Begin building our partial domain to check rules with:
+            string checkAgainst = string.Empty;
+
+            //  Our 'matches' collection:
+            List<TLDRule> ruleMatches = new List<TLDRule>();
+
+            foreach (string domainPart in lstDomainParts)
+            {
+                //  Add on our next domain part:
+                checkAgainst = string.Format("{0}.{1}", domainPart, checkAgainst);
+
+                //  If we end in a period, strip it off:
+                if (checkAgainst.EndsWith("."))
+                    checkAgainst = checkAgainst.Substring(0, checkAgainst.Length - 1);
+
+                var rules = Enum.GetValues(typeof(TLDRule.RuleType)).Cast<TLDRule.RuleType>();
+                foreach (var rule in rules)
+                {
+                    //  Try to match rule:
+                    TLDRule result;
+                    if (TLDRulesCache.Instance.TLDRuleLists[rule].TryGetValue(checkAgainst, out result))
+                    {
+                        ruleMatches.Add(result);
+                    }
+                    Debug.WriteLine(string.Format("Domain part {0} matched {1} {2} rules", checkAgainst, result == null ? 0 : 1, rule));
+                }
+            }
+
+            //  Sort our matches list (longest rule wins, according to :
+            var results = from match in ruleMatches
+                          orderby match.Name.Length descending
+                          select match;
+
+            //  Take the top result (our primary match):
+            TLDRule primaryMatch = results.Take(1).SingleOrDefault();
+
+            if (primaryMatch != null)
+            {
+                Debug.WriteLine(
+                    string.Format("Looks like our match is: {0}, which is a(n) {1} rule.", primaryMatch.Name, primaryMatch.Type)
+                    );
+            }
+            else
+            {
+                Debug.WriteLine(
+                    string.Format("No rules matched domain: {0}", domainString)
+                    );
+            }
+
+            return primaryMatch;
+        } 
+        #endregion
+    }
+}
---
+++ DomainParser.Tests/DomainTests.cs
@@ -0,0 +1,156 @@
+﻿using System.Diagnostics;
+using DomainParser.Library;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DomainParser.Tests
+{
+    /// <summary>
+    /// Summary description for DomainTests
+    /// </summary>
+    [TestClass]
+    public class DomainTests
+    {
+        private DomainName outDomain = null;
+
+        public DomainTests()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void ParseNormalDomain()
+        {
+            //  Try parsing a 'normal' domain:
+            DomainName.TryParse("photos.totallycool.com", out outDomain);
+
+            //  The domain should be parsed as 'totallycool'
+            Assert.AreEqual<string>("totallycool", outDomain.Domain);
+
+            Debug.WriteLine(
+                string.Format("Looks like the parsed domain part is: {0}", outDomain.Domain)
+                );
+        }
+
+        [TestMethod]
+        public void ParseWildcardDomain()
+        {
+            //  Try parsing a 'wildcard' domain
+            if (DomainName.TryParse("photos.verybritish.co.uk", out outDomain))
+            {
+                //  The domain should be parsed as 'verybritish'
+                Assert.AreEqual<string>("verybritish", outDomain.Domain);
+
+                //  The TLD is 'co.uk'
+                Assert.AreEqual<string>("co.uk", outDomain.TLD);
+
+                //  The SLD is just an alias for 'Domain':
+                Assert.AreEqual<string>(outDomain.Domain, outDomain.SLD);
+
+                //  The subdomain is everything else to the left of the domain:
+                Assert.AreEqual<string>("photos", outDomain.SubDomain);
+            }
+            else
+            {
+                Debug.WriteLine("Apparently, we couldn't parse photos.verybritish.co.uk");
+            }
+        }
+
+        [TestMethod]
+        public void ParseExceptionDomain()
+        {
+            //  Try parsing an 'exception' domain
+            DomainName.TryParse("photos.wishlist.parliament.uk", out outDomain);
+
+            //  The domain should be parsed as 'parliament'
+            Assert.AreEqual<string>("parliament", outDomain.Domain);
+
+            Debug.WriteLine(
+                string.Format("Looks like the parsed domain part is: {0}", outDomain.Domain)
+                );
+        }
+        
+        [TestMethod]
+        public void ParseNormalDomainWhereTLDOccursInDomain()
+        {
+            //  Try parsing a 'normal' domain where the TLD part also occurs in the domain part
+            DomainName.TryParse("russian.cntv.cn", out outDomain);
+
+            //  The domain should be parsed as 'cntv'
+            Assert.AreEqual<string>("cntv", outDomain.Domain);
+
+            Debug.WriteLine(
+                string.Format("Looks like the parsed domain part is: {0}", outDomain.Domain)
+                );
+        }
+
+        [TestMethod]
+        public void ParseWildcardDomainWhereTLDOccursInDomain()
+        {
+            //  Try parsing a 'wildcard' domain where the TLD part also occurs in the domain part
+            DomainName.TryParse("com.er.com.er", out outDomain);
+
+            //  The domain should be parsed as 'er'
+            Assert.AreEqual<string>("er", outDomain.Domain);
+
+            Debug.WriteLine(
+                string.Format("Looks like the parsed domain part is: {0}", outDomain.Domain)
+                );
+        }
+
+        [TestMethod]
+        public void ParseExceptionDomainWhereTLDOccursInSubdomain()
+        {
+            //  Try parsing an 'exception' domain where the TLD part also occurs in the subdomain part
+            DomainName.TryParse("www.ck.www.ck", out outDomain);
+
+            //  The domain should be parsed as 'www'
+            Assert.AreEqual<string>("www", outDomain.Domain);
+
+            Debug.WriteLine(
+                string.Format("Looks like the parsed domain part is: {0}", outDomain.Domain)
+                );
+        }
+    }
+}
